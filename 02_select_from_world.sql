c508437ee49e9394101cec0525871747bb8dc153
Jithin Das <jithindasad@gmail.com>
2020-05-04 15:31:51 +0530

    fix typo

--- 02_select_from_world.sql
+++ 02_select_from_world.sql
@@ -58,7 +58,7 @@ SELECT name, capital
   FROM world
  WHERE LENGTH(name) = LENGTH(capital)
 
- */ The capital of Sweden is Stockholm. Both words start with the letter 'S'.
+ /* The capital of Sweden is Stockholm. Both words start with the letter 'S'.
 
 Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
 You can use the function LEFT to isolate the first character.
